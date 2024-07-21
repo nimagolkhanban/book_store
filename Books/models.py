12a870457c43ce21cdda70158ea8cc5714ea1f86
Nima Golkhanban <141530927+nimagolkhanban@users.noreply.github.com>
2024-07-21 17:33:55 +0330

    add book and review model

--- Books/models.py
+++ Books/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)
+    genre = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user} rated {self.rating} to {self.book}'
+
+
--- book_store_main/settings.py
+++ book_store_main/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'Books.apps.BooksConfig',
     'Recommendations.apps.RecommendationsConfig'
-    'Auth'
+
 ]
 
 MIDDLEWARE = [
